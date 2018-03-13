23dab9f0d510c3050013871577f9a3c6ffeeefd4
BATCOMPUTER\aadit <aaditdoshi@gmail.com>
2018-03-12 19:28:25 -0700

    Added Basic Archery Stuff

---
+++ Source/Vaudeville/Weapon/Weapon.h
@@ -0,0 +1,28 @@
+// Vaudeville 2018
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class VAUDEVILLE_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWeapon();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
